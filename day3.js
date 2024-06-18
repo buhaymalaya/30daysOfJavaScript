653a11d6b948e6c7b32ab11ff7b26e4be8ea58dc
Buhay Malaya <malayabuhay@gmail.com>
2024-06-18 18:03:28 -0400

    day3 soln

--- day3.js
+++ day3.js
@@ -1,7 +1,5 @@
-// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
-
- 
-
+// Given an integer n, return a counter function. This counter function initially returns n and then 
+// returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 // Example 1:
 
 // Input: 
@@ -29,3 +27,21 @@
 //     0 <= calls.length <= 1000
 //     calls[i] === "call"
 
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+function createCounter(n) {
+    let current = n;
+
+    return function() {
+        return current++;    
+    };
+}
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
